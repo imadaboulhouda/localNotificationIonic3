d3001d3e80fb76afdad6df4d6cdc2b5f0d98fead
IMAD ABOULHOUDA <imadaboulhouda1@gmail.com>
2019-04-29 09:37:43 +0200

    last

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavController, Platform } from 'ionic-angular';
+import { LocalNotifications } from '@ionic-native/local-notifications';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private localNotifications: LocalNotifications,private platform:Platform) {
+    this.platform.ready().then(() => {
+      this.localNotifications.requestPermission().then(e => {
+        this.notif()
+      }).catch(er => alert(er));
+    });
+
 
   }
-
+  notif()
+  {
+    this.localNotifications.schedule({
+      text: 'Delayed ILocalNotification',
+      led: 'FF0000',
+      trigger: { at: new Date(new Date().getTime() + 10000) },
+      sound: null
+    });
+  }
 }
